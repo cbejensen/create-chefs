428637ed5bef48ac1652ea0c7a2c72c2195018cc
Christian Jensen <cbejensen@gmail.com>
2019-10-18 10:15:13 -0600

    added some images

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ImageGrid from './ImageGridContainer';
-import images from './GalleryImages';
+import React from 'react'
+import ImageGrid from './ImageGridContainer'
+import images from './GalleryImages'
+// import { getImages } from '../../utils/firebaseHelpers'
 
 const Gallery = props => {
+  // getImages()
   const styles = {
     title: {
-      textAlign: 'center'
-    }
-  };
+      textAlign: 'center',
+    },
+  }
   return (
     <div>
       <h1 style={styles.title}>Gallery</h1>
       <ImageGrid images={images} />
     </div>
-  );
-};
+  )
+}
 
-export default Gallery;
+export default Gallery
